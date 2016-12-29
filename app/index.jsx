ee09e7d2444d253f23a905d448b867f3f188a8dc
Jeremy Moore <archtechfl@gmail.com>
2016-12-29 13:33:28 -0500

    adding props

--- app/index.jsx
+++ app/index.jsx
@@ -3,35 +3,50 @@ import React from 'react';
 
 class Header extends React.Component {
 
+	componentDidMount(){
+		console.log("Header mounted");
+	}
+
 	render(){
 	  	return (
-	  		<div>Header goes here</div>
+	  		<div>
+	  			<h1>Customized greeting follows:</h1>
+	  			<p>{this.props.greeting}</p>
+	  		</div>
 	  	);
-	  }
+	}
 
 }
 
 class Footer extends React.Component {
 
+	componentDidMount(){
+		console.log("Footer mounted");
+	}
+
 	render(){
 	  	return (
 	  		<div>Footer goes here</div>
 	  	);
-	  }
+	}
 
 }
 
 class BaseStation extends React.Component {
 
-  render(){
-  	return (
-  		<div>
-	  		<Header />
-	  		<div>Hello, user! I've been expecting you.</div>
-	  		<Footer />
-	  	</div>
-  	);
-  }
+	componentDidMount(){
+		console.log("Base mounted");
+	}
+
+	render(){
+	  	return (
+	  		<div>
+		  		<Header greeting="Jeremy's First React from Scratch"/>
+		  		<div>Hello, user! I've been expecting you.</div>
+		  		<Footer />
+		  	</div>
+	  	);
+  	}
 
 }
 
