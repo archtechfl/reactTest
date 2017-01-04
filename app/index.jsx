a5934d2b85704f5f2cbfcc240746766e029ac08c
Jeremy Moore <archtechfl@gmail.com>
2017-01-04 12:16:06 -0500

    now using babelrc

--- app/index.jsx
+++ app/index.jsx
@@ -28,7 +28,9 @@ class BaseStation extends React.Component {
                 <Header greeting="Jeremy's First React from scratch"/>
                 <TextInput updateUserName={this.updateUserName.bind(this)} label="Enter your name here"/>
                 <RichTextBlock text={"Hello, " + this.state.user + "! I've been expecting you."}/>
-                <Footer copyright="©2016-2017 Jeremy Moore. Licensed under MIT licence."/>
+                <RichTextBlock text="Here I am demonstrating my skill at creating an original React JS build with Webpack."/>
+                <RichTextBlock text="The <span>.babelrc</span> file is now being used for the babel-loader settings."/>
+                <Footer copyright="©2016-2017 Jeremy Moore."/>
             </div>
         );
     }
